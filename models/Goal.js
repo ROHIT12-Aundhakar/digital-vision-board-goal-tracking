6039bdfe05f7c4e259cd05e1f75c047da10f710e
Rohit <rohitaundhakar2003@gmail.com>
2026-05-11 12:56:55 +0530

    Fixed Goal model

--- models/Goal.js
+++ models/Goal.js
@@ -2,26 +2,35 @@ const mongoose = require("mongoose");
 
 const goalSchema = new mongoose.Schema({
 
-  title:{
-    type:String,
-    required:true
+  title: {
+    type: String,
+    required: true
   },
 
-  description:{
-    type:String,
-    required:true
+  description: {
+    type: String,
+    required: true
   },
 
-  image:{
-    type:String
+  image: {
+    type: String,
+    default: ""
   },
 
-  progress:{
-    type:Number,
-    default:0
+  deadline: {
+    type: String,
+    default: ""
+  },
+
+  progress: {
+    type: Number,
+    default: 0
   }
 
 });
 
 module.exports =
-mongoose.model("Goal", goalSchema);
+  mongoose.model(
+    "Goal",
+    goalSchema
+  );
